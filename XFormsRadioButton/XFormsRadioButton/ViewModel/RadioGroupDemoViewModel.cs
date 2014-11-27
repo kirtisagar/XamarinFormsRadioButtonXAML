fa52804c716ebb172841bfcf549e71b1e6fb4a32
kirtisagar <kirtisagar@gmail.com>
2014-11-27 23:14:32 +0530

    updated selected index

--- XFormsRadioButton/XFormsRadioButton/ViewModel/RadioGroupDemoViewModel.cs
+++ XFormsRadioButton/XFormsRadioButton/ViewModel/RadioGroupDemoViewModel.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace XFormsRadioButton.ViewModel
 {
